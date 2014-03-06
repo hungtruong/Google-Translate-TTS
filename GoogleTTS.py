e2dfd227f195b1763809a5a1ae8a2f19f5864d0d
julzhk <jharley@vam.ac.uk>
2014-03-06 16:33:49 +0000

    allow mp3 generator to be called as a function with a dict as arguments

--- GoogleTTS.py
+++ GoogleTTS.py
@@ -5,16 +5,22 @@ import argparse
 import re
 import urllib, urllib2
 import time
+from collections import namedtuple
 
+audio_args = namedtuple('audio_args',['language','output'])
 
-def audio_extract(args=None, input_text=''):
-    if not args:
-        args = {
-          'language':'en',
-          'output':'output.mp3',
-          'file':'text.txt',
-
-        }
+def audio_extract(input_text='',args=None):
+    # This accepts :
+    #   a dict,
+    #   an audio_args named tuple
+    #   or arg parse object
+    if args is None:
+        args = audio_args(language='en',output=open('output.mp3', 'w'))
+    if type(args) is dict:
+        args = audio_args(
+                    language=args.get('language','en'),
+                    output=open(args.get('filename','output.mp3'), 'w')
+        )
     #process input_text into chunks
     input_text = input_text.replace('\n', '')
     text_list = re.split('(\,|\.)', input_text)
@@ -41,12 +47,16 @@ def audio_extract(args=None, input_text=''):
     #download chunks and write them to the output file
     for idx, val in enumerate(combined_text):
         mp3url = "http://translate.google.com/translate_tts?tl=%s&q=%s&total=%s&idx=%s" % (
-        args.language, urllib.quote(val), len(combined_text), idx)
+            args.language,
+            urllib.quote(val),
+            len(combined_text),
+            idx)
         headers = {"Host": "translate.google.com",
                    "Referer": "http://www.gstatic.com/translate/sound_player2.swf",
                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) "
                                  "AppleWebKit/535.19 (KHTML, like Gecko) "
-                                 "Chrome/18.0.1025.163 Safari/535.19"}
+                                 "Chrome/18.0.1025.163 Safari/535.19"
+        }
         req = urllib2.Request(mp3url, '', headers)
         sys.stdout.write('.')
         sys.stdout.flush()
@@ -61,36 +71,40 @@ def audio_extract(args=None, input_text=''):
     print('Saved MP3 to %s' % args.output.name)
 
 
-def main():
-    description='Google TTS Downloader.'
+def text_to_speech_mp3_argparse():
+    description = 'Google TTS Downloader.'
     parser = argparse.ArgumentParser(description=description,
                                      epilog='tunnel snakes rule')
-
-    parser.add_argument('-o','--output',
-                        action='store',nargs='?',
+    parser.add_argument('-o', '--output',
+                        action='store', nargs='?',
                         help='Filename to output audio to',
                         type=argparse.FileType('w'), default='out.mp3')
-    parser.add_argument('-l','--language',
+    parser.add_argument('-l', '--language',
                         action='store',
                         nargs='?',
-                        help='Language to output text to.',default='en')
+                        help='Language to output text to.', default='en')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f','--file',
+    group.add_argument('-f', '--file',
                        type=argparse.FileType('r'),
                        help='File to read text from.')
     group.add_argument('-s', '--string',
                        action='store',
                        nargs='+',
                        help='A string of text to convert to speech.')
-    if len(sys.argv)==1:
-       parser.print_help()
-       sys.exit(1)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
     args = parser.parse_args()
     if args.file:
-        text = args.file.read()
+        input_text = args.file.read()
     if args.string:
-        text = ' '.join(map(str,args.string))
-    audio_extract(args, text)
+        input_text = ' '.join(map(str, args.string))
+    audio_extract(input_text=input_text, args=args)
+
 
 if __name__ == "__main__":
-    main()
+    text_to_speech_mp3_argparse()
+
+# or we can call
+#
+# audio_extract(input_text='boot',args = {'language':'en','output':'outputto.mp3'})
