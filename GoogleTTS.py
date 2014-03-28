35fdc11383b264e56476d142faae36dd631d39bf
Thomas Burette <burettethomas@gmail.com>
2014-03-28 15:33:25 +0100

    Chunks send to google are as big as possible and never over the limit

--- GoogleTTS.py
+++ GoogleTTS.py
@@ -7,37 +7,62 @@ import urllib, urllib2
 import time
 from collections import namedtuple
 
-audio_args = namedtuple('audio_args',['language','output'])
 
 def split_text(input_text, max_length=100):
     """
-    Google TTS only accepts up to (and including) 100 characters long texts.
-    Split the text in chuncks of maximum 100 characters long.
     Try to split between sentences to avoid interruptions mid-sentence.
+    Failing that, split between words.
+    See split_text_rec
     """
-    input_text = input_text.replace('\n', '')
-    text_list = re.split('(\,|\.)', input_text)
-    combined_text = []
-    for idx, val in enumerate(text_list):
-        if idx % 2 == 0:
-            combined_text.append(val)
-        else:
-            joined_text = ''.join((combined_text.pop(), val))
-            if len(joined_text) <= max_length:
-                combined_text.append(joined_text)
+    def split_text_rec(input_text, regexps, max_length=max_length):
+        """
+        Split a string into substrings which are at most max_length.
+        Tries to make each substring as big as possible without exceeding
+        max_length.
+        Will use the first regexp in regexps to split the input into
+        substrings.
+        If it it impossible to make all the segments less or equal than
+        max_length with a regexp then the next regexp in regexps will be used
+        to split those into subsegments.
+        If there are still substrings who are too big after all regexps have
+        been used then the substrings, those will be split at max_length.
+
+        Args:
+            input_text: The text to split.
+            regexps: A list of regexps.
+                If you want the separator to be included in the substrings you
+                can add parenthesis around the regular expression to create a
+                group. Eg.: '[ab]' -> '([ab])'
+
+        Returns:
+            a list of strings of maximum max_length length.
+        """
+        if(len(input_text) <= max_length): return [input_text]
+
+        #mistakenly passed a string instead of a list
+        if isinstance(regexps, basestring): regexps = [regexps]
+        regexp = regexps.pop(0) if regexps else '(.{%d})' % max_length
+
+        text_list = re.split(regexp, input_text)
+        combined_text = []
+        #first segment could be >max_length
+        combined_text.extend(split_text_rec(text_list.pop(0), regexps, max_length))
+        for val in text_list:
+            current = combined_text.pop()
+            concat = current + val
+            if(len(concat) <= max_length):
+                combined_text.append(concat)
             else:
-                subparts = re.split('( )', joined_text)
-                temp_string = ""
-                temp_array = []
-                for part in subparts:
-                    temp_string = temp_string + part
-                    if len(temp_string) > 80:
-                        temp_array.append(temp_string)
-                        temp_string = ""
-                #append final part
-                temp_array.append(temp_string)
-                combined_text.extend(temp_array)
-    return combined_text
+                combined_text.append(current)
+                #val could be >max_length
+                combined_text.extend(split_text_rec(val, regexps, max_length))
+        return combined_text
+
+    return split_text_rec(input_text.replace('\n', ''),
+                          ['([\,|\.|;]+)', '( )'])
+
+
+audio_args = namedtuple('audio_args',['language','output'])
 
 def audio_extract(input_text='',args=None):
     # This accepts :
@@ -52,7 +77,10 @@ def audio_extract(input_text='',args=None):
                     output=open(args.get('output','output.mp3'), 'w')
         )
     #process input_text into chunks
+    #Google TTS only accepts up to (and including) 100 characters long texts.
+    #Split the text in segments of maximum 100 characters long.
     combined_text = split_text(input_text)
+
     #download chunks and write them to the output file
     for idx, val in enumerate(combined_text):
         mp3url = "http://translate.google.com/translate_tts?tl=%s&q=%s&total=%s&idx=%s" % (
@@ -103,17 +131,13 @@ def text_to_speech_mp3_argparse():
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
-    args = parser.parse_args()
+    return parser.parse_args()
+    
+
+if __name__ == "__main__":
+    args = text_to_speech_mp3_argparse()
     if args.file:
         input_text = args.file.read()
     if args.string:
         input_text = ' '.join(map(str, args.string))
     audio_extract(input_text=input_text, args=args)
-
-
-if __name__ == "__main__":
-    text_to_speech_mp3_argparse()
-
-# or we can call
-#
-# audio_extract(input_text='boot',args = {'language':'en','output':'outputto.mp3'})
